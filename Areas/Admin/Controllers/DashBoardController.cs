f02c7d589d77b1fef1cc88aa009798f09292c314
antoine <bromade8300@gmail.com>
2025-06-11 18:23:07 +0200

    init portfolio

---
+++ Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testPortfolio.Areas.Admin.Controllers
+
+{
+
+    [Area("Admin")]
+    public class DashBoardController : Controller
+    {
+        // GET: AdminController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
