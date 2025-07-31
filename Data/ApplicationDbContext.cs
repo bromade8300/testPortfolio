9f93f8dcc38cf18208f59140ab20b8b95abbe9c8
DESKTOP-SJ5TUOJ\antoine <bromade8300@gmail.com>
2025-07-31 18:04:25 +0200

    refactor(dbContext, Services page) : finish migration and useless model Card

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,7 +7,4 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
-
-   public DbSet<testPortfolio.Models.Product> Products { get; set; }
-   public DbSet<testPortfolio.Models.Picture> Pictures { get; set; }
 }
